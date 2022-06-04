f31baf3391181fc4a1f446955893f7f5dc31d37e
Kdot117 <kizaguir@uncc.edu>
2022-06-04 07:59:50 -0400

    updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,16 @@ import kenny from '../components/kenny.jpg';
 
 
 export default function Header () {
+  
 
     return (
+        
         <container className="art-bg">
             <div className="pattern">
                 <img src={kenny} className="img" alt="logo" height="400" length="400"/>
+                
                 <h2 className="name-tag">&lt;Kendrick/&gt;</h2>
+               
             </div>
 
             
